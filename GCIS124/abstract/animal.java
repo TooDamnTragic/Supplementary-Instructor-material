3aadd8493f6c27958c1ba008ef7f2d40296311f6
Zane <mb9257@rit.edu>
2025-01-08 01:37:57 -0500

    animals

--- GCIS124/abstract/animal.java
+++ GCIS124/abstract/animal.java
@@ -8,38 +8,38 @@ public abstract class Animal {
     public abstract void makeSound();
 }
 
-public class Dog extends Animal {
-    public Dog(String name) {
-        super(name);
-    }
+// public class Dog extends Animal {
+//     public Dog(String name) {
+//         super(name);
+//     }
 
 
-}
+// }
 
 
-public abstract class Vehicle {
-    public abstract void drive();
-}
+// public abstract class Vehicle {
+//     public abstract void drive();
+// }
 
-public class Tester {
-    public static void main(String[] args) {
-        Vehicle v = new Vehicle();  // Attempt to instantiate
-        v.drive();
-    }
-}
+// public class Tester {
+//     public static void main(String[] args) {
+//         Vehicle v = new Vehicle();  // Attempt to instantiate
+//         v.drive();
+//     }
+// }
 
-public abstract class Shape {
-    public abstract double area();
-}
+// public abstract class Shape {
+//     public abstract double area();
+// }
 
-public class Circle extends Shape {
-    private double radius;
+// public class Circle extends Shape {
+//     private double radius;
 
-    public Circle(double radius) {
-        this.radius = radius;
-    }
+//     public Circle(double radius) {
+//         this.radius = radius;
+//     }
 
-    // MISTAKE:
-    public double area(double extra) {
-        return Math.PI * radius * radius + extra;
-    }
+//     // MISTAKE:
+//     public double area(double extra) {
+//         return Math.PI * radius * radius + extra;
+//     }
