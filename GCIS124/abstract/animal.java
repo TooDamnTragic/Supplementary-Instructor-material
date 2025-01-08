f59ef51f59d79b87c608b4d34cdfeaadcdd4ecd1
Zane <mb9257@rit.edu>
2025-01-08 01:36:16 -0500

    unit3, polymoephisms

---
+++ GCIS124/abstract/animal.java
@@ -0,0 +1,45 @@
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void makeSound();
+}
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+
+}
+
+
+public abstract class Vehicle {
+    public abstract void drive();
+}
+
+public class Tester {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();  // Attempt to instantiate
+        v.drive();
+    }
+}
+
+public abstract class Shape {
+    public abstract double area();
+}
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // MISTAKE:
+    public double area(double extra) {
+        return Math.PI * radius * radius + extra;
+    }
