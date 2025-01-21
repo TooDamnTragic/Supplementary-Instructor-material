22115c7ad9673051f65a4357ab0ad676ffc7ae7f
Zane <mb9257@rit.edu>
2025-01-21 16:55:20 -0500

    k

--- GCIS124/Unit1.java
+++ GCIS124/Unit1.java
@@ -2,11 +2,11 @@ public class Unit1 {
     public static void main(String[] args) {
         System.out.println(13 + '4');
 
-        // System.out.println(1 - 4.0);
+        System.out.println(1 - 4.0);
         
-        // System.out.println(14 / 5);
+        System.out.println(14 / 5);
         
-        // System.out.println(7 / 5.0);
+        System.out.println(7 / 5.0);
 
         // System.out.println(9 // 4);
 
