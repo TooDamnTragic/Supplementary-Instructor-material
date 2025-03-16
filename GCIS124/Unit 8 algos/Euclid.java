0e20b0c493b7bd57a101d421073308ab199ad121
Zane <mb9257@rit.edu>
2025-03-16 03:24:16 -0400

    euclid

---
+++ GCIS124/Unit 8 algos/Euclid.java
@@ -0,0 +1,16 @@
+public class Euclid {
+    public static int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("GCD of 252 and 105: " + gcd(252, 105));
+    }
+}
